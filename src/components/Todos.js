60df22b1d9796322c6d2c389d1e6d2314c5ac2da
anmolraidewan <anmoldewanrai@gmail.com>
2021-11-12 19:23:41 +0530

    first commit

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,92 +1,85 @@
-import {useEffect, useState} from 'react';
-import {firestore} from '../fbConfig/fbConfig';
-import trashIcon from '../icons/delete.svg';
-import addIcon from '../icons/plus.svg';
+import { useEffect, useState } from "react";
+import { firestore } from "../fbConfig/fbConfig";
+import trashIcon from "../icons/delete.svg";
+import addIcon from "../icons/plus.svg";
 
-const collection = firestore.collection('todos');
+const collection = firestore.collection("todos");
 
 export default function Todos() {
-
   const [todos, setTodos] = useState([]);
   const [title, setTitle] = useState("");
   const [loading, setLoading] = useState(true);
 
   // Retrieve Firestore Todos
-  useEffect(() =>{
-    collection.onSnapshot(snapshot =>{
+  useEffect(() => {
+    collection.onSnapshot((snapshot) => {
+      console.log(snapshot);
       const fbData = [];
-      snapshot.forEach(doc => {
-        fbData.push(({...doc.data(), id: doc.id}))
+      snapshot.forEach((doc) => {
+        fbData.push({ ...doc.data(), id: doc.id });
       });
-      setTodos(fbData); 
+      setTodos(fbData);
       setLoading(false);
-    })
-  }, [])
+    });
+  }, []);
 
   // Add New Todo to Firestore
-  const newTodo = (title) =>{
-    collection.add({ title })
-    setTitle('');
-  }
+  const newTodo = (title) => {
+    collection.add({ title });
+    setTitle("");
+  };
 
-  const handleSubmit = (e) =>{
+  const handleSubmit = (e) => {
     e.preventDefault();
     newTodo(title);
-  }
+  };
 
   // Delete a todo
-  const deleteTodo = (id) =>{
+  const deleteTodo = (id) => {
     collection.doc(id).delete();
-  }
+  };
 
   return (
     <div className="todo-component">
-      <h1 className="heading">
-        Todo(s)
-        </h1>
-      <form 
-      className="todo-form"
-      onSubmit={handleSubmit}>
+      <h1 className="heading">Todo(s)</h1>
+      <form className="todo-form" onSubmit={handleSubmit}>
         <input
-        autoFocus
-        placeholder="Add a new todo" 
-        className="todo-input"
-        type="text"
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-        required
-        />    
-        <input 
-        className="icon-add"
-        type="image" src={addIcon}/>
+          autoFocus
+          placeholder="Add a new todo"
+          className="todo-input"
+          type="text"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          required
+        />
+        <input className="icon-add" type="image" src={addIcon} />
       </form>
-      {loading ? (<p className="loading">Loading ...</p>) : 
-      // if loading is false execute code below
-      (<ul className="todo-list">
-        { todos.length ? todos.map(todo =>{
-          return(
-            <div 
-            data-id={todo.id}
-            key={todo.id} 
-            className="todo-item">
-              <p
-              className="todo-title">
-                {todo.title}
-              </p>
-              <img className="icons icon-delete"
-                src={trashIcon} alt="trashIcon"
-                id={todo.id}
-                draggable="false  "
-                onClick={(e) => deleteTodo(e.target.id)}/>
-            </div>
-          )
-        }) :
-        <p className="no-todo">
-          No todo today!
-        </p>
-        }
-      </ul>)
-}
+      {loading ? (
+        <p className="loading">Loading ...</p>
+      ) : (
+        // if loading is false execute code below
+        <ul className="todo-list">
+          {todos.length ? (
+            todos.map((todo) => {
+              return (
+                <div data-id={todo.id} key={todo.id} className="todo-item">
+                  <p className="todo-title">{todo.title}</p>
+                  <img
+                    className="icons icon-delete"
+                    src={trashIcon}
+                    alt="trashIcon"
+                    id={todo.id}
+                    draggable="false"
+                    onClick={(e) => deleteTodo(e.target.id)}
+                  />
+                </div>
+              );
+            })
+          ) : (
+            <p className="no-todo">No todo today!</p>
+          )}
+        </ul>
+      )}
     </div>
-  )
+  );
 }
